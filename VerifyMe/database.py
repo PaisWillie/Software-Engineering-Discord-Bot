d4bd3451f2bff791ad2695330cc3a0acfd2e4baf
Luigi Quattrociocchi <lquatt2@gmail.com>
2021-07-08 16:37:55 -0400

    VerifyMe database implement creating database from classlist csv files

--- VerifyMe/database.py
+++ VerifyMe/database.py
@@ -1,18 +1,34 @@
 import sqlite3
 import os
+import csv
+from collections import namedtuple
 
 
 class Database:
-    DB_KEYS = ("macid", "is_verified", "stream", "specialty",
-               "discord_name", "discord_id", "is_TA", "is_upper_year")
-    DB_TYPES = ("text", "integer", "text", "text",
-                "text", "text", "integer", "integer")
+    DB_KEYS = ("macid", "full_name", "is_verified", "stream",
+               "specialty", "discord_name", "discord_id", "is_TA")
+    DB_TYPES = ("text", "text", "integer", "text",
+                "text", "text", "text", "integer")
     DB_PROTOTYPE = str(tuple(f"{key} {type}" for key, type in zip(
         DB_KEYS, DB_TYPES))).replace('\'', '')
 
+
+    UserData = namedtuple("UserData", " ".join(DB_KEYS))
+
     @staticmethod
-    def new_user(macid):
-        return macid, 0, None, None, None, None, 0, 0
+    def new_user(macid, full_name, stream, is_TA) -> UserData:
+        return Database.UserData(macid, full_name, 0, stream, None, None, None, is_TA)
+
+    @staticmethod
+    def from_classlist(filepath, stream) -> list[UserData]:
+        users = []
+        with open(filepath, "r", newline='') as csv_in:
+            reader = csv.reader(csv_in, delimiter=',', quotechar="\"")
+            for image, full_name, macid, role in reader:
+                is_TA = int(role != "Student")
+                users.append(Database.new_user(macid, full_name, stream, is_TA))
+
+        return users
 
     class User(dict):
         def __init__(self, database, values):
@@ -33,7 +49,7 @@ class Database:
             self.database.cache[self["macid"]] = self
             return super().__setitem__(key, value)
 
-    def create_db(self, macids):
+    def create_db(self, users : list[tuple]):
         if os.path.isfile(self.path):
             return False
 
@@ -41,8 +57,7 @@ class Database:
         cur = conn.cursor()
 
         cur.execute(f"CREATE TABLE users {Database.DB_PROTOTYPE}")
-        cur.executemany(f"INSERT INTO users VALUES (?{', ?' * (len(Database.DB_KEYS) - 1)})",
-                        (Database.new_user(macid) for macid in macids))
+        cur.executemany(f"INSERT INTO users VALUES (?{', ?' * (len(Database.DB_KEYS) - 1)})", users)
 
         conn.commit()
         cur.close()
@@ -72,3 +87,14 @@ class Database:
 
     def __contains__(self, item):
         return self.__getitem__(item) != None
+
+
+def main():
+    db = Database("VerifyMe/users.db")
+    user_data = list(set(
+        Database.from_classlist("VerifyMe/classlists/2DA4.csv", "Software") +
+        Database.from_classlist("VerifyMe/classlists/2GA3.csv", "Software")))
+    db.create_db(user_data)
+
+if __name__ == '__main__':
+    main()
